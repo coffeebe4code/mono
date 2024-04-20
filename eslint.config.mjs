0184b32c65477fd5fd1e39931fd4054dad66d32d
Chris <chris.clark@coffeeb4code.com>
2024-04-20 07:23:15 -0500

    ran eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,12 +5,12 @@ import jest from 'eslint-plugin-jest';
 import globals from 'globals';
 
 export default [
-  ...js.configs.recommended,
+  js.configs.recommended,
   {
     files: ['src/**/*.spec.js'],
     ...jest.configs['flat/recommended'],
   },
-  ...jsdoc.configs['flat/recommended'],
+  jsdoc.configs['flat/recommended'],
   {
     files: ['src/**/*.js'],
     languageOptions: { globals: { ...globals.node } },
